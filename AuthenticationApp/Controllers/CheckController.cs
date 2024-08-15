b0a584f2b2a12fc7486750c417bf893d4d8123d9
jenifer1409 <103604140+jenifer1409@users.noreply.github.com>
2024-08-15 17:00:25 -0400

    Add files via upload

---
+++ AuthenticationApp/Controllers/CheckController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckController : ControllerBase
+    {
+
+        [HttpGet("throw")]
+        public int ThrowException()
+        {
+            var b = 10;
+            var c = 0;
+            var a = 0;
+            try
+            {               
+                a = b / c;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("This is test exception");
+
+            }
+            return a;
+        }
+    }
+}
---
+++ AuthenticationApp/ExceptionMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+
+namespace AuthenticationApp
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+
+        public Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+
+            _logger.LogError(exception, "Unexpected error in your code");
+
+            var response = new
+            {
+                statuscode = (int)HttpStatusCode.InternalServerError,
+                Message = "Critical !!!! High Alert ...Internal server error"
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return context.Response.WriteAsJsonAsync(response);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
+
+
